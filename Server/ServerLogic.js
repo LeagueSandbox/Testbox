14e82c073b47f2c19039d8fa1a9bcb7532ef6ea7
MatthewFrench <matthewf@ksu.edu>
2016-12-16 18:18:43 -0800

    LeagueUI now pulls every start and once every 5 minutes

--- Server/ServerLogic.js
+++ Server/ServerLogic.js
@@ -4,35 +4,68 @@ var LobbyManager = require('./LobbyManager');
 function ServerLogic() {
 	this.networkManager = new NetworkManager(this);
 	this.lobbyManager = new LobbyManager(this);
+
+	var updateServerTimer;
+	updateServerTimer = CreateFunction(this, function() {
+        this.updateGameServer("", function(){
+            console.log("Success updating server!");
+        });
+        setTimeout(updateServerTimer, 1000 * 60 * 5);
+    });
+	updateServerTimer();
 }
 
-ServerLogic.prototype.startGameServer = function(gameJSON, port, callback) {
+ServerLogic.prototype.updateGameServer = function(gameJSON, callback) {
     const exec = require('child_process').spawn;
-    const game = exec('GameServerApp.exe', ['--port', port, '--configJSON', JSON.stringify(gameJSON)], {cwd: '../../GameServer/GameServerApp/bin/Release'});
 
+    //--gameServerRepository "https://github.com/LeagueSandbox/GameServer.git" --repositoryBranch "master" --commitMessageName "LastCommitMessage.txt" --gameServerSourceFileName "GameServer Source" --copyBuildToFolder "Compiled GameServer" --needsCopied false --pauseAtEnd true --configJSON ""
+    const gameUpdater = exec('AutoCompilerForGameServer.exe', ['--gameServerRepository', "https://github.com/LeagueSandbox/GameServer.git", '--repositoryBranch', "master", '--commitMessageName', "LastCommitMessage.txt", '--gameServerSourceFileName', "GameServer Source", '--copyBuildToFolder', "Compiled GameServer", '--needsCopied', 'true', '--pauseAtEnd', 'false', '--configJSON', JSON.stringify(gameJSON)],
+        {cwd: '../Game-Server-Repositories'});
 
-    var waitingForBoot = true;
-    game.stdout.on('data', (data) => {
-
+    gameUpdater.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
-        if (waitingForBoot) {
-            if (data.indexOf("Game is ready.") !== -1) {
-                console.log("Game is ready, doing callback");
-                waitingForBoot = false;
-                callback();
-            }
-        }
 
     });
 
-    game.stderr.on('data', (data) => {
+    gameUpdater.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
     });
 
-    game.on('close', (code) => {
+    gameUpdater.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
+        callback();
     });
+};
 
+ServerLogic.prototype.startGameServer = function(gameJSON, port, callback) {
+    this.updateGameServer(gameJSON, CreateFunction(this, function () {
+        const exec = require('child_process').spawn;
+
+        const game = exec('GameServerApp.exe', ['--port', port, '--configJSON', JSON.stringify(gameJSON)],
+            {cwd: '../Game-Server-Repositories/GameServer/GameServerApp/bin/Release'});
+
+        var waitingForBoot = true;
+        game.stdout.on('data', (data) => {
+
+            console.log(`stdout: ${data}`);
+            if (waitingForBoot) {
+                if (data.indexOf("Game is ready.") !== -1) {
+                    console.log("Game is ready, doing callback");
+                    waitingForBoot = false;
+                    callback();
+                }
+            }
+
+        });
+
+        game.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+
+        game.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+    }));
 };
 
 var serverInstance = new ServerLogic();
